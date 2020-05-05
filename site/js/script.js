9819895eae510cac4a0b82130495462e9ef55e47
Elisabetta Pagliara <pagliara.elisabetta@gmail.com>
2020-05-05 20:31:32 +0200

    button

--- site/js/script.js
+++ site/js/script.js
@@ -1,32 +1,35 @@
-function sayHELLO () {
-	var comment= document.getElementById("buttons").value;
-	var comment2=" <h4>Thanks a lot for your feedback : " + comment +" We are happy that you appreciated it :) </h4>";
 
-	this.textContent="Conferm done !" //change the text after click the button
+document.addEventListener("DOMContentLoaded",
+ function (event) {
+	function sayHELLO () {
+		var comment= document.getElementById("buttons").value;
+		var comment2=" <h4>Thanks a lot for your feedback : " + comment +" We are happy that you appreciated it :) </h4>";
 
-    //document.getElementById("result").textContent=comment2;
-    //document.querySelector("#result").textContent=comment2;
-    
+		this.textContent="Conferm done !" //change the text after click the button
 
-    if ( comment >5 && comment <=10  ){
+	    //document.getElementById("result").textContent=comment2;
+	    //document.querySelector("#result").textContent=comment2;
+	    
 
-    		document.getElementById("result").innerHTML=comment2;
-    }
-   
-    else if (comment<5){
-    	var comment3=" <h4>Thanks for your feedback : " + comment +". We are sorry that we have disappointed you, we will keep improving, hoping to increase your score ! </h4>";
-    	document.getElementById("result").innerHTML=comment3;
-    
+	    if ( comment >5 && comment <=10  ){
 
-    }
-    else if (comment > 10){
- 			var comment4=" <h4> We were not expecting to have such an impact on you, eheh ! </h4>";
-    	document.getElementById("result").innerHTML=comment4;
-    
+	    		document.getElementById("result").innerHTML=comment2;
+	    }
+	   
+	    else if (comment<5){
+	    	var comment3=" <h4>Thanks for your feedback : " + comment +". We are sorry that we have disappointed you, we will keep improving, hoping to increase your expectations ! </h4>";
+	    	document.getElementById("result").innerHTML=comment3;
+	    
+
+	    }
+	    else if (comment > 10){
+	 			var comment4=" <h4> We were not expecting to have such an impact on you, eheh ! </h4>";
+	    	document.getElementById("result").innerHTML=comment4;
+	    
 
 
-    }
-}
-//event handler
+	    }
+	}
+	//event handler
 
-document.querySelector("button").addEventListener("click",sayHELLO);
+	document.querySelector("button").addEventListener("click",sayHELLO); })
