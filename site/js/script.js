8d38099c26087722e60ad8ef59c47ff313e92ff1
Elisabetta Pagliara <pagliara.elisabetta@gmail.com>
2020-05-05 18:15:26 +0200

    button

--- site/js/script.js
+++ site/js/script.js
@@ -1,11 +1,22 @@
 function sayHELLO () {
 	var comment= document.getElementById("button").value;
-	var comment2=" <h4>Thanks for your feedback : " + comment +"</h4>";
+	var comment2=" <h4>Thanks a lot for your feedback : " + comment +" We are happy that you appreciated iy </h4>";
 
 
 
     //document.getElementById("result").textContent=comment2;
+    //document.querySelector("#result").textContent=comment2;
+    
 
-    document.getElementById("result").innerHTML=comment2;
+    if ( comment >5 ){
 
+    		document.getElementById("result").innerHTML=comment2;
+    }
+   
+    else {
+    	var comment3=" <h4>Thanks for your feedback : " + comment +" We are sossry that we have disappointed you, we will kepp improving, hoping to increase your score ! </h4>";
+    	document.getElementById("result").innerHTML=comment3;
+    
+
+    }
 }
