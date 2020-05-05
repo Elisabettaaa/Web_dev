b1ef99ab68c4e912b0abcdda6b5a8abc5f69aaf1
Elisabetta Pagliara <pagliara.elisabetta@gmail.com>
2020-05-05 18:02:39 +0200

    button

--- site/js/script.js
+++ site/js/script.js
@@ -1,9 +1,9 @@
-function sayHELLO(){
+function sayHELLO () {
 	var comment= document.getElementById("button").value;
 	var comment2="Thanks for the feedback : " + comment ;
 
 
 
-    document.getElementById("result").textContent=cooment2;
+    document.getElementById("result").textContent=comment2;
 
 }
